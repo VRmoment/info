16748ed519a893ea3153c845e86eb06a7851c7f6
Yu-En Weng <rrayw13@gmail.com>
2015-11-11 23:09:08 +0800

    update
    v2

--- js/index.js
+++
@@ -1,6 +0,0 @@
-$('document').ready(function() {
-	var options = { videoId: 'E8r8euxmYBw', start: 3 };
-	$('top').tubular(options);
-	// f-UGhWj1xww cool sepia hd
-	// 49SKbS7Xwf4 beautiful barn sepia
-});
